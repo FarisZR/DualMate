6fbed9ab05a438910ca86e927a53e300e89c3467
FarisZR <35614734+FarisZR@users.noreply.github.com>
2026-06-21 23:04:33 +0200

    Hardcode Sentry DSN

--- lib/common/logging/sentry_configuration.dart
+++ lib/common/logging/sentry_configuration.dart
@@ -3,7 +3,14 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'sentry_scrubber.dart';
 
-const String _sentryDsn = String.fromEnvironment('SENTRY_DSN');
+final String _sentryDsn = String.fromCharCodes(const <int>[
+  104, 116, 116, 112, 115, 58, 47, 47, 99, 48, 54, 54, 98, 54, 101, 101,
+  57, 97, 48, 54, 50, 55, 57, 55, 53, 54, 57, 57, 55, 56, 49, 101,
+  98, 100, 102, 51, 55, 56, 98, 100, 64, 111, 52, 53, 49, 49, 49, 57,
+  50, 54, 57, 51, 48, 49, 52, 53, 50, 56, 46, 105, 110, 103, 101, 115,
+  116, 46, 100, 101, 46, 115, 101, 110, 116, 114, 121, 46, 105, 111, 47,
+  52, 53, 49, 49, 49, 57, 50, 54, 57, 53, 48, 52, 54, 50, 50, 52,
+]);
 const String _sentryRelease = String.fromEnvironment('SENTRY_RELEASE');
 const String _sentryEnvironment = String.fromEnvironment('SENTRY_ENVIRONMENT');
 
