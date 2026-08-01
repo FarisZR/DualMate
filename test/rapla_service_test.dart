73f771a06da53ed8c9737ca55007bb1f8ef423b3
FarisZR <35614734+FarisZR@users.noreply.github.com>
2026-08-01 02:02:52 +0200

    Refactor RaplaScheduleSource querySchedule method and update URL validation tests for new Karlsruhe host

--- test/rapla_service_test.dart
+++ test/rapla_service_test.dart
@@ -2,9 +2,16 @@ import 'package:dualmate/schedule/service/rapla/rapla_schedule_source.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('debugging', () async {
+  test('accepts the legacy Karlsruhe Rapla calendar URL', () {
     const raplaUrl =
-        "https://rapla.dhbw-karlsruhe.de/rapla?page=calendar&user=eisenbiegler&file=TINF24B4";
+        'https://rapla.dhbw-karlsruhe.de/rapla?page=calendar&user=eisenbiegler&file=TINF24B4';
+
+    expect(RaplaScheduleSource.isValidUrl(raplaUrl), isTrue);
+  });
+
+  test('accepts the new Karlsruhe Rapla calendar URL', () {
+    const raplaUrl =
+        'https://rapla.dhbw.de/rapla/calendar?user=ritterbusch%40dhbw-karlsruhe.aa&file=TINF25B2';
 
     expect(RaplaScheduleSource.isValidUrl(raplaUrl), isTrue);
   });
